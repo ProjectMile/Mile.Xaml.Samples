60fb924df9cb668e9b5ae788904e55340932e956
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-01-02 21:12:43 +0800

    Add MileXamlBlankApp project.

---
+++ MileXamlBlankApp/App.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+
+#include "App.h"
+#include "App.g.cpp"
+#include "MainPage.h"
+
+using namespace winrt;
+using namespace Windows::ApplicationModel;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::Foundation;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace MileXamlBlankApp;
+using namespace MileXamlBlankApp::implementation;
+
+namespace winrt::MileXamlBlankApp::implementation
+{
+    App::App()
+    {
+        Initialize();
+    }
+}
