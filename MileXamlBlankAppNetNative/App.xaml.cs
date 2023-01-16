3d903db270260a2b048a172428f9c5e188dde838
AndromedaMelody <57533055+AndromedaMelody@users.noreply.github.com>
2023-01-16 23:28:56 +0800

    Add MileXamlBlankAppNetNative project. (#1)

---
+++ MileXamlBlankAppNetNative/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Mile.Xaml;
+
+namespace MileXamlBlankAppNetNative
+{
+    sealed partial class App : Application
+    {
+        public App()
+        {
+            this.Initialize();
+        }
+    }
+}
