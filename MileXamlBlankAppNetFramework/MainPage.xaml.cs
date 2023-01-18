263b6ab0c7693938de7a248c9cb7483ab1a4a62b
AndromedaMelody <57533055+AndromedaMelody@users.noreply.github.com>
2023-01-18 09:02:43 +0800

    Add MileXamlBlankAppNetFramework project. (#2)

---
+++ MileXamlBlankAppNetFramework/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MileXamlBlankAppNetFramework
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs _)
+        {
+            (sender as Button).Content = "Clicked";
+        }
+    }
+}
