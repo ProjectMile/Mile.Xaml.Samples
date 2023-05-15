d09b901bcbc7f3da9dc929fe6984bf2f6d034964
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-05-16 01:24:20 +0800

    Update Mile.Xaml to 2.0.611.

--- MileXamlBlankAppNetFramework/App.xaml.cs
+++ MileXamlBlankAppNetFramework/App.xaml.cs
@@ -1,23 +1,20 @@
-﻿using Windows.UI.Xaml;
-using Windows.UI.Xaml.Hosting;
+﻿using Mile.Xaml;
+using Windows.UI.Xaml;
 
 namespace MileXamlBlankAppNetFrameworkModern
 {
     sealed partial class App : Application
     {
-        WindowsXamlManager windowsXamlManager = null;
-
         public App()
         {
-            this.windowsXamlManager = WindowsXamlManager.InitializeForCurrentThread();
+            this.ThreadInitialize();
             this.InitializeComponent();
         }
 
         public void Close()
         {
             this.Exit();
-            this.windowsXamlManager.Dispose();
-            this.windowsXamlManager = null;
+            this.ThreadUninitialize();
         }
     }
 }
