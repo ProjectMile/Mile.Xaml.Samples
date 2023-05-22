a61a75f4bdb8ef9fee4a8882f04acf5106398416
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-05-22 22:08:51 +0800

    Enable XAML Islands content transparent support for MileXamlBlankAppNetFramework.

--- MileXamlBlankAppNetFramework/App.xaml.cs
+++ MileXamlBlankAppNetFramework/App.xaml.cs
@@ -1,13 +1,40 @@
 ﻿using Mile.Xaml;
+using System;
+using System.Runtime.InteropServices;
 using Windows.UI.Xaml;
 
 namespace MileXamlBlankAppNetFrameworkModern
 {
+    [ComImport]
+    [Guid("06636C29-5A17-458D-8EA2-2422D997A922")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIInspectable)]
+    public interface IWindowPrivate
+    {
+        bool TransparentBackground { get; set; }
+    }
+
     sealed partial class App : Application
     {
+        internal static IWindowPrivate GetIWindowPrivateFromCurrentCoreWindow()
+        {
+            IntPtr CoreWindowIntPtr = Marshal.GetIUnknownForObject(Window.Current);
+            try
+            {
+                return (IWindowPrivate)Marshal.GetTypedObjectForIUnknown(
+                    CoreWindowIntPtr,
+                    typeof(IWindowPrivate));
+            }
+            finally
+            {
+                Marshal.Release(CoreWindowIntPtr);
+            }
+        }
+
         public App()
         {
             this.ThreadInitialize();
+            var Yolo = GetIWindowPrivateFromCurrentCoreWindow();
+            Yolo.TransparentBackground = true;
             this.InitializeComponent();
         }
 
