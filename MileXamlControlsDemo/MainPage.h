7cefd67695ce2c60a82aa724509c3197fdf97b07
高怡飞 <363301617@qq.com>
2023-07-01 11:07:20 +0800

    Update Mile.Xaml Sun Valley new controls demo (#4)

--- MileXamlControlsDemo/MainPage.h
+++ MileXamlControlsDemo/MainPage.h
@@ -2,10 +2,13 @@
 
 #include "MainPage.g.h"
 
+#include "winrt/Windows.Foundation.Collections.h"
+
 namespace winrt::MileXamlControlsDemo::implementation
 {
     struct MainPage : MainPageT<MainPage>
     {
+    public:
         MainPage() = default;
 
         void InitializeComponent();
@@ -14,6 +17,15 @@ namespace winrt::MileXamlControlsDemo::implementation
         void MyProperty(int32_t value);
 
         void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void HyperlinkButton_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Windows::UI::Xaml::RoutedEventArgs const&);
+        void HyperlinkButton_Click1(winrt::Windows::Foundation::IInspectable const&, winrt::Windows::UI::Xaml::RoutedEventArgs const&);
+
+        winrt::Windows::Foundation::Collections::IObservableVector<winrt::MileXamlControlsDemo::TreeViewItemModel> TreeViewDataList();
+        void TreeViewDataList(winrt::Windows::Foundation::Collections::IObservableVector<winrt::MileXamlControlsDemo::TreeViewItemModel> value);
+
+    private:
+        winrt::Windows::Foundation::Collections::IObservableVector<winrt::MileXamlControlsDemo::TreeViewItemModel> _treeViewDataList = winrt::single_threaded_observable_vector<winrt::MileXamlControlsDemo::TreeViewItemModel>();
+
     };
 }
 
---
+++ MileXamlControlsDemo/TreeViewItemModel.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "TreeViewItemModel.g.h"
+
+#include "winrt/Windows.Foundation.Collections.h"
+
+namespace winrt::MileXamlControlsDemo::implementation
+{
+    struct TreeViewItemModel : TreeViewItemModelT<TreeViewItemModel>
+    {
+    public:
+        TreeViewItemModel() = default;
+
+        bool IsSelected();
+        void IsSelected(bool value);
+
+        winrt::hstring Name();
+        void Name(winrt::hstring value);
+
+        winrt::Windows::Foundation::Collections::IObservableVector<winrt::MileXamlControlsDemo::TreeViewItemModel> ChildrenNode();
+        void ChildrenNode(winrt::Windows::Foundation::Collections::IObservableVector<winrt::MileXamlControlsDemo::TreeViewItemModel> value);
+
+    private:
+        bool _isSelected = false;
+        winrt::hstring _name = L"";
+        winrt::Windows::Foundation::Collections::IObservableVector<winrt::MileXamlControlsDemo::TreeViewItemModel> _childrenNode = winrt::single_threaded_observable_vector<winrt::MileXamlControlsDemo::TreeViewItemModel>();
+    };
+}
+
+namespace winrt::MileXamlControlsDemo::factory_implementation
+{
+    struct TreeViewItemModel : TreeViewItemModelT<TreeViewItemModel, implementation::TreeViewItemModel>
+    {
+    };
+}
