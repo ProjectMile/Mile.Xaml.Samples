0db0c509ec6da39f5c107b2ddef6fb7e853fa503
高怡飞 <363301617@qq.com>
2024-07-25 20:19:14 +0800

    Add SystemBackdrop samples for Mile.Xaml
    Add SystemBackdrop samples for Mile.Xaml

---
+++ MileXamlBlankAppNetFramework/UI/Backdrop/DesktopAcrylicKind.cs
@@ -0,0 +1,11 @@
+﻿namespace MileXamlBlankAppNetFrameworkModern.UI.Backdrop
+{
+    public enum DesktopAcrylicKind
+    {
+        Default = 0,
+
+        Base = 1,
+
+        Thin = 2
+    }
+}
---
+++ MileXamlBlankAppNetFramework/WindowsAPI/PInvoke/Comctl32/SUBCLASSPROC.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using MileXamlBlankAppNetFrameworkModern.WindowsAPI.PInvoke.User32;
+
+namespace MileXamlBlankAppNetFrameworkModern.WindowsAPI.PInvoke.Comctl32
+{
+    public delegate IntPtr SUBCLASSPROC(IntPtr hWnd, WindowMessage uMsg, UIntPtr wParam, IntPtr lParam, uint uIdSubclass, IntPtr dwRefData);
+}
