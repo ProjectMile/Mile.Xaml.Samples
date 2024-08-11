0ff761fb79c6dc3dcfff073cbbfd01263dac5e75
driver1998 <driver1998.ms@outlook.com>
2024-08-12 06:42:41 +0800

    Add sample for .NET 8 Mile.Xaml App (#7)

---
+++ MileXamlBlankAppNetCore/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MileXamlBlankAppNetCore
+{
+
+    public sealed partial class MainPage : Page
+    {
+        public string FrameworkDescription => RuntimeInformation.FrameworkDescription;
+        public bool IsDynamicCodeSupported => RuntimeFeature.IsDynamicCodeSupported;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var contentDialog = new ContentDialog
+            {
+                
+                Title = "Hello world",
+                Content = "Where do you want to go today?",
+                PrimaryButtonText = "OK",
+                XamlRoot = (sender as Button).XamlRoot,
+            };
+            await contentDialog.ShowAsync();
+        }
+    }
+}
