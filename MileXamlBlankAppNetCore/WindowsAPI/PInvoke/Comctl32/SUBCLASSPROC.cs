6bb3aa9b14006598e1eb1a6c8148188f7e1ccbd0
高怡飞 <363301617@qq.com>
2024-09-03 10:16:43 +0800

    Add SystemBackdrop samples for Modern .NET
    Add SystemBackdrop samples for Modern .NET

---
+++ MileXamlBlankAppNetCore/WindowsAPI/PInvoke/Comctl32/SUBCLASSPROC.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using MileXamlBlankAppNetCoreModern.WindowsAPI.PInvoke.User32;
+
+namespace MileXamlBlankAppNetCoreModern.WindowsAPI.PInvoke.Comctl32
+{
+    public delegate IntPtr SUBCLASSPROC(IntPtr hWnd, WindowMessage uMsg, UIntPtr wParam, IntPtr lParam, uint uIdSubclass, IntPtr dwRefData);
+}
---
+++ MileXamlBlankAppNetCore/WindowsAPI/PInvoke/Kernel32/Kernel32Library.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MileXamlBlankAppNetCoreModern.WindowsAPI.PInvoke.Kernel32
+{
+    public static partial class Kernel32Library
+    {
+        private const string Kernel32 = "kernel32.dll";
+
+        public const long APPMODEL_ERROR_NO_PACKAGE = 15700L;
+
+        [LibraryImport(Kernel32, EntryPoint = "GetSystemPowerStatus", SetLastError = false)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static partial bool GetSystemPowerStatus(out SYSTEM_POWER_STATUS lpSystemPowerStatus);
+    }
+}
